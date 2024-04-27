bd0da72519a95d08883c1d9d7702ffa0927d8692
Marlen272 <marlen.04.29.07@gmail,com>
2024-04-27 18:07:26 +0600

    Описание вашего коммита

--- js.js
+++ js.js
@@ -1,36 +1,52 @@
-// //1ЗАДАНИЕ
-// const regExp = /^[1-9]+$/
-//
-// const containsOnlyDigits = (str) => {
-//     return regExp.test(str)
+//1
+// const extractNumbers = (skr) => {
+//     return skr.match(/\d+/g).map(Number)
 // }
-//
-// console.log(containsOnlyDigits("12345")) // Выведет true
-// console.log(containsOnlyDigits("12a45")) // Выведет false
-// console.log(containsOnlyDigits("129k45")) // Выведет false
-// console.log(containsOnlyDigits("12345324")) // Выведет true
-//
-// //2ЗАДАНИЕ
-// function printSecond() {
-//
-//     const intervalId = setInterval(() => {
-//         console.log(`Прошла секунда `)}, 1000)
-//     setTimeout(() => {
-//         clearInterval(intervalId)}, 5000)}
-// printSecond()
-//
-// //3ЗАДАНИЕ
-//
-//
-//     const count = (i) => {
-//     console.log(i)
-//         if (i < 10) {setTimeout(() => count(i + 1), 1000)}}
-// count(1)
-
-//4ЗАДАНИЕ
+// console.log(extractNumbers("a1fg5hj6")) // Вернёт [1, 5, 6]
 
 
+//2
+// const fibonachi = (n, a = 0, b = 1) => {
+//     if (n > 0) {
+//         console.log(a)
+//         setTimeout(() => fibonachi(n - 1, b, a + b), 1000)
+//     }
+// }
+// fibonachi(13)
 
+
+//3
+// const fetchData = async () => {
+//     const response = await fetch('https://fakestoreapi.com/products')
+//     const data = await response.json()
+//     const titles = data.map(item => item.title)
+//     console.log(titles)
+// }
+// fetchData().catch(error => console.error('Ошибка получения данных:', error))
+
+
+//4
+// const colorButtons = document.querySelectorAll('#colorButtons button');
+// colorButtons.forEach(button => {
+//     button.addEventListener('click', function() {
+//         const colorClass = this.classList[0]
+//         document.body.className = colorClass
+//     })
+// })
+
+
+//6
+// document.addEventListener('DOMContentLoaded', function() {
+//     const counterElement = document.getElementById('counter');
+//     let count = 0;
+//     const intervalId = setInterval(function() {
+//         count++
+//         counterElement.textContent = count
+//         if (count >= 100) {
+//             clearInterval(intervalId)
+//         }
+//     }, 1) // интервал в 1 миллисекунду
+// })
 
 
 
